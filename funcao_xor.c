b11d7ccc5d310a107e42b9c3ead33118189105c6
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2017-05-25 22:18:55 -0300

    O C nao tem a funcao OR exclusiva, mas pode ser facilmente criada com as funcoes logicas existentes

---
+++ funcao_xor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+	Retorna a operacao logica XOR usando dois argumentos
+	O Operador XOR retorna verdadeiro se, e somente se, um dos dois argumentos for verdade
+*/
+int xor(int a, int b){
+	return (a || b) && !(a && b);
+}
+
+int main(){
+	
+	printf("%d\n", xor(1, 0));
+	printf("%d\n", xor(1, 1));
+	printf("%d\n", xor(0, 1));
+	printf("%d\n", xor(0, 0));
+
+	return 0;
+}
